aefcee8b18abea160bed29b2090eb10419ecbc06
Darshan D <darshandmysuru@gmail.com>
2023-10-29 12:14:36 +0530

    made changes part 1

---
+++ weaher/script.js
@@ -0,0 +1,30 @@
+const apiKey = '89719c63f20e2950dfd76274a2541bca';
+const city = 'Mysuru';
+
+
+const weatherDescription = document.getElementById('weather-description');
+const temperature = document.getElementById('temperature');
+const foodSuggestion = document.getElementById('food-suggestion');
+
+async function getWeatherData() {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
+        const data = await response.json();
+
+        weatherDescription.textContent = data.weather[0].description;
+        temperature.textContent = `Temperature: ${data.main.temp}°C`;
+        
+        // Food suggestion based on weather conditions
+        if (data.weather[0].main === 'Clear') {
+            foodSuggestion.textContent = 'Enjoy a picnic with fresh fruits and sandwiches!';
+        } else if (data.weather[0].main === 'Clouds' || data.weather[0].main === 'Rain') {
+            foodSuggestion.textContent = 'Warm soup and a cup of coffee would be great!';
+        } else {
+            foodSuggestion.textContent = 'Why not try a barbecue today?';
+        }
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+    }
+}
+
+getWeatherData();
